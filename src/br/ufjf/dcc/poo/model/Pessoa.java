8a5e57c7319c2680b8ae72426e7b7b961698a700
ice <ice@lab06m35.nrc.ice.ufjf.br>
2023-02-01 21:52:11 -0300

    01/02/2023 4

---
+++ src/br/ufjf/dcc/poo/model/Pessoa.java
@@ -0,0 +1,37 @@
+package br.ufjf.dcc.poo.model;
+
+public abstract class Pessoa {
+	private String nome;
+	private int numero;
+	private String complemento;
+	private String bairro;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	
+	
+
+}
